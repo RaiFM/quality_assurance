ef0bf27e1670a183a3da3bf68ecc1ba891268f7e
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-11-25 17:05:36 -0300

    create axample

---
+++ testes_unitarios/pedido-sistema/src/main/java/com/example/pedido/ItemDoPedido.java
@@ -0,0 +1,24 @@
+package com.example.pedido;
+
+public class ItemDoPedido {
+
+    private Produto produto;
+    private int quantidade;
+
+    public ItemDoPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
